2c677160d5f7016f3ea9d25bed3fdfd17e752911
Baz Chalk <boruch11@gmail.com>
2025-06-06 11:54:13 +0300

    correct github pages next setup

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: "/selfhydro",
+  images: {
+    unoptimized: true,
+  },
+  assetPrefix: "/selfhydro/",
 };
 
 export default nextConfig;
